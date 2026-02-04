02bb1baa51397a6a06fa4c431414f3512ea651ac
danielcoilacenteno2004 <danielcoilacenteno@gmail.com>
2026-02-04 13:19:38 -0500

    Cambios menores de organización en venta_de_grupos.php

--- inc/core/setup.php
+++ inc/core/setup.php
@@ -3,21 +3,33 @@
  * Core Setup: Carga de Assets y Soportes del Tema
  */
 
-// 1. CARGA DE ESTILOS Y SCRIPTS
 function semin_scripts_modular() {
-    // Globales
+    // 1. Carga Global (Siempre necesaria)
     wp_enqueue_style( 'semin-global', get_stylesheet_uri(), array(), '3.1' );
     wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' );
 
-    // Condicionales
-    if ( is_front_page() ) wp_enqueue_style( 'css-front-page', get_template_directory_uri() . '/css/front-page.css', array('semin-global'), '1.0' );
-    if ( is_page_template('page-nosotros.php') ) wp_enqueue_style( 'css-page-nosotros', get_template_directory_uri() . '/css/page-nosotros.css', array('semin-global'), '1.0' );
-    if ( is_page_template('page-proyectos.php') || is_singular('proyecto') || is_post_type_archive('proyecto') ) wp_enqueue_style( 'css-page-proyectos', get_template_directory_uri() . '/css/page-proyectos.css', array('semin-global'), '1.0' );
-    if ( is_page_template('page-grupos.php') ) wp_enqueue_style( 'css-page-grupos', get_template_directory_uri() . '/css/page-grupos.css', array('semin-global'), '1.0' );
-    if ( is_page_template('page-contacto.php') ) wp_enqueue_style( 'css-page-contacto', get_template_directory_uri() . '/css/page-contacto.css', array('semin-global'), '1.0' );
-    if ( is_page_template('page-servicios.php') ) wp_enqueue_style( 'css-page-servicios', get_template_directory_uri() . '/css/page-servicios.css', array('semin-global'), '1.0' );
+    // 2. 🧠 CARGADOR DINÁMICO DE CSS (AUTOMÁTICO)
+    // Obtiene el archivo PHP que WordPress decidió usar para esta página actual
+    global $template;
 
+    // Extrae el nombre limpio del archivo (ej: 'C:/.../page-venta-de-grupos.php' -> 'page-venta-de-grupos')
+    $nombre_plantilla = basename( $template, '.php' );
 
+    // Define dónde debería estar el CSS correspondiente
+    // Busca en: /wp-content/themes/semin-theme/css/[nombre-plantilla].css
+    $ruta_relativa_css = '/css/' . $nombre_plantilla . '.css';
+    $ruta_absoluta_css = get_template_directory() . $ruta_relativa_css;
+
+    // Pregunta: ¿Existe ese archivo CSS?
+    if ( file_exists( $ruta_absoluta_css ) ) {
+        // ¡Sí existe! Cárgalo dinámicamente.
+        wp_enqueue_style( 
+            'css-auto-' . $nombre_plantilla, // ID único (ej: css-auto-page-venta-de-grupos)
+            get_template_directory_uri() . $ruta_relativa_css, 
+            array('semin-global'), 
+            '1.0' 
+        );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'semin_scripts_modular' );
 
